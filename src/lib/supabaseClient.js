6a27739b29c2e53dfa2bcc2e84d149b7571595f4
Sn0wF1re <mokayaclinton34@gmail.com>
2024-08-07 13:44:51 +0300

    Configure supabase
    - configure the client
    - move sensitive data to .env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://bwrrikjvegcshdsajvcc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3cnJpa2p2ZWdjc2hkc2FqdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3NzA0MjYsImV4cCI6MjAzODM0NjQyNn0.tEzGCe1htgeY-Hm6bHdXlLVGfUwrC2TRMx99Wp-Atq0')
+const supabase_url = 'https://bwrrikjvegcshdsajvcc.supabase.co';
+const supabase_key = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabase_url, supabase_key);
