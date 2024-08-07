f7a366fcb2c5b13271a8eac84ebf5067824cfadb
Sn0wF1re <mokayaclinton34@gmail.com>
2024-08-07 13:52:15 +0300

    Create main page for tasks
    - create display for tasks
    - create add task form
    - create controls for filtering tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,3 +1,113 @@
 <template>
-    hello world
+    <div class="user-">
+        <h2>Welcome, {{ last_name }}! Here are the controls for your tasks &#128071</h2>
+    </div>
+    <div class="controls">
+        <button @click="showForm=true" class="add-button">Add task</button>
+        <button @click="setFilterType('all')" class="all">All</button>
+        <button @click="setFilterType('favorites')" class="fav">Favorites</button>
+        <button @click="setFilterType('completed')" class="complete">Completed</button>
+        <button @click="setFilterType('pending')" class="pending">pending</button>
+        <!-- <p>{{ taskStore.user_id }}</p> -->
+    </div>
+
+    <div class="add-task" v-if="showForm">
+        <input v-model="taskToAdd" type="text" placeholder="Add a task" />
+        <button @click="createTask">Add task</button>
+    </div>
+
+    <div class="tasks">
+        <TaskCard v-for="task in filteredTasks" :task="task" :key="task.id" />
+    </div>
 </template>
+
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import TaskCard from '../components/TaskCard.vue';
+import { useTaskStore } from '../stores/taskStore';
+
+
+const taskStore = useTaskStore();
+const { tasks } = storeToRefs(taskStore);
+const taskToAdd = ref('');
+const showForm = ref(false);
+const filterType = ref('all');
+const { last_name } = storeToRefs(taskStore);
+
+const createTask = () => {
+    taskStore.createTask(taskToAdd.value);
+    taskToAdd.value = '';
+    showForm.value = false;
+};
+
+const setFilterType = (type) => {
+    filterType.value = type;
+};
+
+const filteredTasks = computed(() => {
+    if (filterType.value === 'all') {
+        return tasks.value;
+    } else if (filterType.value === 'favorites') {
+        return tasks.value.filter(task => task.favorite);
+    } else if (filterType.value === 'completed') {
+        return tasks.value.filter(task => task.completed);
+    } else if (filterType.value === 'pending') {
+        return tasks.value.filter(task => !task.completed);
+    }
+});
+
+onMounted(async () => {
+    await taskStore.fetchTasks();
+    await taskStore.initializeUserId();
+});
+</script>
+
+<style scoped>
+.controls {
+    .complete {
+        background-color: green;
+    }
+
+    .pending {
+        background-color: yellow;
+    }
+
+    .fav {
+        background-color: red;
+    }
+
+    .all {
+        background-color: blue;
+    }
+
+    .add-button {
+        background-color: blue;
+    }
+}
+
+.add-task {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 2rem 0;
+
+    input {
+        padding: 0.5rem;
+        margin-right: 1rem;
+        outline: none;
+    }
+
+    button {
+        padding: 0.5rem 1rem;
+    }
+}
+
+.tasks {
+    margin-top: 2rem;
+}
+
+button {
+    margin: 0 0.5rem;
+}
+</style>
