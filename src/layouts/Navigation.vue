2c4b953eef0b76ab8555c3a2b96e187edd68215e
Sn0wF1re <mokayaclinton34@gmail.com>
2024-08-07 13:44:12 +0300

    Create navigation layout for main page

---
+++ src/layouts/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="nav">
+        <h1>What's Next?</h1>
+        <button @click="handleLogOut">Log out</button>
+    </nav>
+
+    <slot />
+</template>
+
+<script setup>
+import { supabase } from '../lib/supabaseClient';
+import router from '../router';
+
+const handleLogOut = async () => {
+    try {
+        const { error } = await supabase.auth.signOut();
+
+        if (error) {
+            console.log('Error encountered while logging out:', error);
+        } else {
+            router.push('/login');
+        }
+    } catch (error) {
+        console.log('Error encountered while logging out:', error);
+    }
+};
+</script>
+
+<style scoped>
+.nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 1rem;
+    padding-bottom: 2rem;
+    margin-bottom: 2rem;
+    box-shadow: 0 4px 2px -2px rgba(0, 0, 0, 0.1);
+
+    button {
+        background-color: red;
+    }
+}
+</style>
