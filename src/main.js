27914d2e35a942ea6ab197cc18315a0fbb2ad55e
Sn0wF1re <mokayaclinton34@gmail.com>
2025-01-02 11:54:23 +0300

    Add quasar to project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
+import { Quasar, Dialog } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {
+        Dialog
+    }
+})
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
+// FILE: vite.config.js
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar()
+  ]
 })
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
